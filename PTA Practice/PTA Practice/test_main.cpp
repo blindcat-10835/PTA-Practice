e58cb266e1b3001083a49a14bdd1eb9efd06eb01
blind cat <a1083550183@qq.com>
2018-08-19 01:20:00 +0800

    1004 complete

--- PTA Practice/PTA Practice/test_main.cpp
+++ PTA Practice/PTA Practice/test_main.cpp
@@ -1,60 +1,35 @@
 #include <iostream>
 #include <string>
+#define MAX 1000
 using namespace std;
+
 int main(int argc, char const *argv[])
 {
-	int n = 0, p = 0, a = 0, b = 0, c = 0, t = 0;
-	bool isYES = true;
-	cin >> n;
-	string s;
-	while (n--)
+	int length = 0, score[MAX];
+	string name[MAX], number[MAX];
+	cin >> length;
+	int i = length;
+	while (i--)
 	{
-		cin >> s;
-		a = 0, p = 0, b = 0, t = 0, c = 0;
-		for (int j = 0; j < s.length(); j++)
+		cin >> name[i] >> number[i] >> score[i];
+	}
+	int max = 0, min = 101, m = 0, n = 0;
+	i = length;
+	while (i--)
+	{
+		if (score[i] > max)
 		{
-			/*notice!!!!!!!
-			 if string('P') replaced by string("P").there would be error;
-			 why??????????
-			*/
-			if (s[j] == 'A')
-			{
-				if (p == 0)
-				{
-					a++;
-				}
-				else if (p == 1 && t == 0)
-				{
-					b++;
-				}
-				else if (p == 1 && t == 1)
-				{
-					c++;
-				}
-			}
-			else if (s[j] == 'P'&&p == 0)
-			{
-				p++;
-			}
-			else if (s[j] == 'T'&&t == 0 && p == 1 && b >= 1)
-			{
-				t++;
-			}
-			else
-			{
-				isYES = false;
-				break;
-			}
+			max = score[i];
+			m = i;
 		}
-		if (p == 1 && c == a * b && t == 1)
+		if (score[i] < min)
 		{
-			cout << "YES" << endl;
-		}
-		else
-		{
-			cout << "NO" << endl;
+			min = score[i];
+			n = i;
 		}
 	}
+	cout << name[m] << " " << number[m] << endl;
+	cout << name[n] << " " << number[n] << endl;
 
 	return 0;
 }
