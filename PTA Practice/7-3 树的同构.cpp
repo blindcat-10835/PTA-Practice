61276387b6a93186e090d0bb320f4e6acb33caf2
blind cat <a1083550183@qq.com>
2018-09-10 01:15:55 +0800

    add 7-3树的同构，7-4 是否同一个BST
    考试惨败！！！加油复习基础！

---
+++ PTA Practice/7-3 树的同构.cpp
@@ -0,0 +1,159 @@
+﻿/*
+7-3 树的同构（25 point(s)）
+给定两棵树T1和T2。如果T1可以通过若干次左右孩子互换就变成T2，则我们称两棵树是“同构”的。例如图1给出的两棵树就是同构的，因为我们把其中一棵树的结点A、B、G的左右孩子互换后，就得到另外一棵树。而图2就不是同构的。
+
+现给定两棵树，请你判断它们是否是同构的。
+输入格式:
+输入给出2棵二叉树树的信息。对于每棵树，首先在一行中给出一个非负整数N (≤10)，即该树的结点数（此时假设结点从0到N−1编号）；随后N行，第i行对应编号第i个结点，给出该结点中存储的1个英文大写字母、其左孩子结点的编号、右孩子结点的编号。如果孩子结点为空，则在相应位置上给出“-”。给出的数据间用一个空格分隔。注意：题目保证每个结点中存储的字母是不同的。
+
+输出格式:
+如果两棵树是同构的，输出“Yes”，否则输出“No”。
+
+输入样例1（对应图1）：
+8
+a 1 2
+B 3 4
+C 5 -
+D - -
+E 6 -
+G 7 -
+F - -
+H - -
+8
+G - 4
+B 7 6
+F - -
+A 5 1
+H - -
+C 0 -
+D - -
+E 2 -
+输出样例1:
+Yes
+输入样例2（对应图2）：
+8
+B 5 7
+F - -
+A 0 3
+C 6 -
+H - -
+D - -
+G 4 -
+E 1 -
+8
+D 6 -
+B 5 -
+E - -
+H - -
+C 0 2
+G - 3
+F - -
+A 1 4
+输出样例2:
+No
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <cctype>
+#include <set>
+#include <queue>
+using namespace std;
+typedef char Elementtype;
+typedef int PtrToTree;
+const int MAXLENGTH = 1000;
+
+//使用数组来表示树，则树根==序号，index == root;
+struct TreeNode
+{
+	Elementtype Value = 0;
+	PtrToTree Left = -1;
+	PtrToTree Right = -1;
+};
+void getTree(vector<TreeNode> &T)
+{
+	int nodeN = T.size();
+	for (int i = 0; i < nodeN; i++)
+	{
+		char left, right;
+		/*if use "%c instead of "%s"  it will ! get '\n'or' '*/
+		scanf("%c %c %c\n", &T[i].Value, &left, &right);
+		if (left != '-')T[i].Left = left - '0';
+		if (right != '-')T[i].Right = right - '0';
+	}
+}
+
+PtrToTree FindElement(vector<TreeNode> T1, char Value)
+{
+	int root = -1;
+	for (int i = 0; i < T1.size(); i++)
+	{
+		if (T1[i].Value == Value)
+		{
+			root = i;
+			break;
+		}
+	}
+	return root;
+}
+PtrToTree FindTreeRoot(vector<TreeNode> T1)
+{
+	int root = -1;
+	vector<bool> hasParent(T1.size(), false);
+	for (TreeNode i : T1)
+	{
+		if (i.Left != -1)hasParent[i.Left] = true;
+		if (i.Right != -1)hasParent[i.Right] = true;
+	}
+	for (int i = 0; i < hasParent.size(); i++)
+	{
+		if (!hasParent[i])
+		{
+			root = i;
+			break;
+		}
+	}
+	return root;
+}
+bool Juge(vector<TreeNode> T1, vector<TreeNode> T2)
+{
+	for (int i = 0; i < T1.size(); i++)
+	{
+		PtrToTree T2_i = FindElement(T2, T1[i].Value);
+		if (T2_i == -1)return false;
+		char LValue1 = '-', RV1 = '-', LV2 = '-', RV2 = '-';
+		if (T2[T2_i].Left != -1)LValue1 = T2[T2[T2_i].Left].Value;
+		if (T2[T2_i].Right != -1)	RV1 = T2[T2[T2_i].Right].Value;
+		if (T1[i].Right != -1)	LV2 = T1[T1[i].Right].Value;
+		if (T1[i].Left != -1)	RV2 = T1[T1[i].Left].Value;
+		if (LValue1 == LV2 && RV1 == RV2)
+		{
+		}
+		else if (LValue1 == RV2 && LV2 == RV1)
+		{
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(int argc, char const *argv[])
+{
+	/*分配树的空间,输入树*/
+	int N1 = 0, N2 = 0;
+	scanf("%d\n", &N1);
+	vector<TreeNode> T1(N1);
+	getTree(T1);
+	scanf("%d\n", &N2);
+	vector<TreeNode> T2(N2);
+	getTree(T2);
+	/*判断是否同构*/
+	if (Juge(T1, T2))cout << "Yes\n";
+	else cout << "No\n";
+	return 0;
+}
--- PTA Practice/PTA Practice/test_main.cpp
+++ PTA Practice/PTA Practice/test_main.cpp
@@ -1,135 +1,123 @@
-//1017	Queueing at Bank	25	710	2809	0.25
-//1018	Public Bike Management	30	920	3695	0.25
-//1021	Deepest Root	25	1059	3908	0.27
-//1025	PAT Ranking	25	1444	5228	0.28
-//1026	Table Tennis	30	372	2210	0.17
-
-//1028	List Sorting	25	1020	3553	0.29
-
-//1032	Sharing	25	1044	3701	0.28
-//1033	To Fill or Not to Fill	25	758	2582	0.29
-//1039	Course List for Student	25	1021	4350	0.23
-//1047	Student List for Course	25	673	4191	0.16
-//1052	Linked List Sorting	25	910	4184	0.22
-//1055	The World's Richest	25	734	5904	0.12
-//1057	Stack	30	637	2199	0.29
-
-//1060	Are They Equal	25	793	3466	0.23
-//1061	Dating	20	754	3394	0.22
-//1065	Score[0] + B and Score[1](64bit)	20	1225	4770	0.26
-//1067	Sort with Swap(0, i)	25	819	2931	0.28
-//1068	Find More Coins	30	492	1749	0.28
-//1072	Gas Station	30	690	2885	0.24
-//1074	Reversing Linked List	25	747	2569	0.29
-//1075	PAT Judge	25	697	3489	0.20
-//1077	Kuchiguse	20	873	4209	0.21
-//1078	Hashing	25	861	3236	0.27
-
-//1080	Graduate Admission	30	736	3307	0.22
-//1081	Rational Sum	20	776	2645	0.29
-//1082	Read Number in Chinese	25	619	2276	0.27
-//1085	Perfect Sequence	25	1026	3739	0.27
-//1088	Rational Arithmetic	20	577	2009	0.29
-//1095	Cars on Campus	30	588	2345	0.25
-//1096	Consecutive Factors	20	700	2685	0.26
-//1101	Quick Sort	25	653	2929	0.22
-
+﻿/*
+1018
+1017
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 #include <cmath>
-#include <algorithm>
-#include <map>
+#include <vector>
+#include <cctype>
+#include <set>
+#include <queue>
 using namespace std;
-int fee[25] = { 0 };
-struct Record
-{
-	string name = "";
-	int month = 0, day = 0, hour = 0, minute = 0, time = 0;
-	bool is_online = false;
-	bool is_vivid = false;
-} rec[1001];
 
-double caculate(int day, int hour, int minute)
+typedef int Elementtype;
+typedef int PtrToTree;
+const int MAXLENGTH = 1000;
+const int null = -1;
+
+//使用数组来表示树，则树根==序号，index == root;
+struct TreeNode
 {
-	double total = fee[hour] * minute + fee[24] * 60 * day;
-	for (int i = 0; i < hour; i++)
-		total += fee[i] * 60;
-	return total / 100.0;
-}
-bool cmp(Record a, Record b)
+	Elementtype Value = null;
+	PtrToTree Left = null;
+	PtrToTree Right = null;
+};
+
+PtrToTree BuildTree(vector<int> Senquence, vector<TreeNode> &Tree, int length)
 {
-	return a.name != b.name ? a.name < b.name : a.time < b.time;
-}
-int main(int argc, char const *argv[])
-{
-	for (int i = 0; i < 24; i++)
+	int root = 0;
+	for (int i = 0; i < length; i++)
 	{
-		scanf("%d", &fee[i]);
-		fee[24] += fee[i];
+		Tree[i].Value = Senquence[i];
 	}
-	int N;
-	scanf("%d", &N);
-	Record t; char t1[10] = "off-line"; char t2[21] = "name";
-	for (int i = 0; i < N; i++)
+	for (int i = 1; i < length; i++)
 	{
-		scanf("%s %d:%d:%d:%d %s", &t2[0], &t.month, &t.day, &t.hour, &t.minute, &t1[0]);
-		t.name = t2;
-		t.time = t.day * 24 * 60 + t.hour * 60 + t.minute;
-		if (t1[1] == 'n')t.is_online = true;
-		else t.is_online = false;
-		rec[i] = t;
-	}
-	sort(rec, rec + N, cmp);
-	string Name = "";
-	int day1, hour1, minute1;
-	double Total_amount = 0.0;
-	for (int i = 1; i < N; i++)
-	{
-		if (rec[i].name == rec[i - 1].name && rec[i - 1].is_online == true && rec[i].is_online == false && rec[i].is_vivid == false)
+		int j = 0;
+		while (true)
 		{
-			rec[i].is_vivid = true;
-			rec[i - 1].is_vivid = true;
-		}
-	}
-	int j = 0;
-	for (int i = 0; i < N; i++)
-	{
-		if (rec[i].is_vivid)rec[j++] = rec[i];
-	}
-	rec[j].name = Name;
-	for (int i = 0; i < j; i++)
-	{
-		if (rec[i].name != Name)
-		{
-			Name = rec[i].name;
-			day1 = rec[i].day, hour1 = rec[i].hour, minute1 = rec[i].minute;
-			printf("%s %02d\n", &Name[0], rec[i].month);
-		}
-		else
-		{
-			if (!rec[i].is_online)
+			if (Senquence[i] < Tree[j].Value)
 			{
-				if (rec[i - 1].is_online)
+				if (Tree[j].Left != null)
 				{
-					int day2 = rec[i].day, hour2 = rec[i].hour, minute2 = rec[i].minute;
-					int mi = (day2 - day1) * 24 * 60 + (hour2 - hour1) * 60 + (minute2 - minute1);
-					double cost = caculate(day2, hour2, minute2) - caculate(day1, hour1, minute1);
-					printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.02f\n", day1, hour1, minute1, day2, hour2, minute2, mi, cost);
-					Total_amount += cost;
+					j = Tree[j].Left;
+				}
+				else
+				{
+					Tree[j].Left = i;
+					break;
 				}
 			}
 			else
 			{
-				day1 = rec[i].day, hour1 = rec[i].hour, minute1 = rec[i].minute;
+				if (Tree[j].Right != null)
+				{
+					j = Tree[j].Right;
+				}
+				else
+				{
+					Tree[j].Right = i;
+					break;
+				}
 			}
 		}
-		if (rec[i].name != rec[i + 1].name)
+	}
+	return root;
+}
+bool isTheSameBST(vector<int> S1, vector<int> S2, int length)
+{
+	vector<TreeNode> Tree(length);
+	int root = BuildTree(S1, Tree, length);
+	vector<bool> isVisited(length, false);
+	for (int i = 0; i < length; i++)
+	{
+		int j = root;
+		while (S2[i] != Tree[j].Value&& j != null)
 		{
-			printf("Total amount: $%.02f\n", Total_amount);
-			Total_amount = 0;
+			if (isVisited[j] == false)return false;
+			if (S2[i] < Tree[j].Value)
+			{
+				j = Tree[j].Left;
+			}
+			else
+			{
+				j = Tree[j].Right;
+			}
+		}
+		isVisited[j] = true;
+	}
+	return true;
+}
+
+int main(int argc, char const *argv[])
+{
+	int N = 10, L;
+	vector<int> Sequence(N), ToBeCompare(N);
+	while (true)
+	{
+		scanf("%d", &N);
+		if (N == 0)break;
+		scanf("%d", &L);
+		for (int i = 0; i < N; i++)
+		{
+			scanf("%d", &Sequence[i]);
+		}
+		for (int i = 0; i < L; i++)
+		{
+			for (int k = 0; k < N; k++)
+			{
+				scanf("%d", &ToBeCompare[k]);
+			}
+			if (isTheSameBST(Sequence, ToBeCompare, N))
+			{
+				cout << "Yes\n";
+			}
+			else
+			{
+				cout << "No\n";
+			}
 		}
 	}
-
 	return 0;
 }
